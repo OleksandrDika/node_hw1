fb0ec4e2fe0ca110c38c4fe43107b82cdab9c0c8
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2023-05-08 21:06:58 +0300

    make hw1

---
+++ index.js
@@ -0,0 +1,41 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./db/contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.log("Unknown action");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "vza2RIzNGIwutCVCs4mCL" });
+// invokeAction({
+//   action: "add",
+//   name: "Taras Shevchenko",
+//   email: "taras25@gmail.com",
+//   phone: "(38) 155-2835",
+// });
+// invokeAction({ action: "remove", id: "vza2RIzNGIwutCVCs4mCL" });
